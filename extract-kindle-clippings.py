669f2074ba973a89c655383a6d853e8933017526
linnabraham <linn.official@gmail.com>
2023-04-08 17:40:05 +0530

    make changes relevant for markdown instead of rst

--- extract-kindle-clippings.py
+++ extract-kindle-clippings.py
@@ -87,11 +87,11 @@ print('Scanning output dir', outpath)
 for directory, subdirlist, filelist in os.walk(outpath):
     for fname in filelist:
         ext = fname[-4:]
-        if ext == '.rst' or ext == '.RST':
-            print('Found RST file', fname, 'in directory', directory)
+        if ext == '.md' or ext == '.MD':
+            print('Found MD file', fname, 'in directory', directory)
             # open file, find commend lines, store hashes
-            rst = open(directory + '/' + fname, 'r')
-            line = rst.readline()
+            md = open(directory + '/' + fname, 'r')
+            line = md.readline()
             lines = 0
             hashes = 0
             while line:
@@ -101,11 +101,11 @@ for directory, subdirlist, filelist in os.walk(outpath):
                     foundhash = findhash_result[0]
                     existing_hashes[foundhash] = fname
                     hashes += 1
-                line = rst.readline()
-            rst.close()
+                line = md.readline()
+            md.close()
             print(hashes, 'hashes found in', lines, 'scanned lines')
         else:
-            print('File', fname, 'does not seem to be RST, skipping', ext)
+            print('File', fname, 'does not seem to be MD, skipping', ext)
 
 print('Found', len(existing_hashes), 'existing note hashes')
 print('Processing clippings file', infile)
@@ -191,10 +191,10 @@ for key in pub_title.keys():
     if len(short_title) > 128:
         short_title = short_title[:127]
     if (nr_notes > 2):
-        fname = author + ' - ' + short_title.strip() + '.rst'
+        fname = author + ' - ' + short_title.strip() + '.md'
         short = 0
     else:
-        fname = 'short_notes.rst'
+        fname = 'short_notes.md'
         short = 1
 
     new_hashes = 0
@@ -218,7 +218,7 @@ for key in pub_title.keys():
         if author != 'Unknown':
             titlestr = author + ' - ' + title
         else:
-            titlestr = title
+            titlestr = "# " + title
         out.write(titlestr + '\n')
         out.write(('-' * len(titlestr)) + '\n\n')
     elif not newfile:
